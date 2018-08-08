26093501f4b47ca84fd74c1792a23ddd60521509
lukasz.wac <lukasz.wac@naturaily.com>
2018-08-08 16:32:25 +0200

    add POST and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    error => {
+      res.status(400).send(error);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
